b941fa0229f6bace2802564e94026b54f1169d2b
MNDMMstudy <107236809+MNDMMstudy@users.noreply.github.com>
2022-07-14 05:30:39 +0000

    [finish]lesson19

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,7 +18,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
    def destroy
      @post_image = PostImage.find(params[:id])
      @post_image.destroy
